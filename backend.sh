3a4f93e01dbcce301e4b58bca37ac976374c35d3
chandureddy08 <chandrareddyerigela@gmail.com>
2024-04-24 17:17:38 -0400

    Expense project

--- backend.sh
+++ backend.sh
@@ -1,57 +1,60 @@
 #!/bin/bash
 
+source ./common.sh
+
+check_root
 
 
 
 dnf module disable nodejs -y &>>LOGFILE
-VALIDATE $? "Disabling Node js"
+# $? "Disabling Node js"
 
 dnf module enable nodejs:20 -y &>>LOGFILE
-VALIDATE $? "Enabling Node js 20"
+# $? "Enabling Node js 20"
 
 dnf install nodejs -y &>>LOGFILE
-VALIDATE $? "Installing Node js"
+# $? "Installing Node js"
 
 id expense
 if [ $? -ne 0 ]
 then
   useradd expense &>>LOGFILE
-  VALIDATE $? "Creating user expense"
+  # $? "Creating user expense"
 else
   echo -e "user expense is already exist...$Y SKIPPING $N"
 fi
 
 mkdir -p /app &>>LOGFILE
-VALIDATE $? "Creating app Directory"
+# $? "Creating app Directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>LOGFILE
-VALIDATE $? "Downloading backend code"
+# $? "Downloading backend code"
 
 cd /app &>>LOGFILE
 rm -rf /app/*
 unzip /tmp/backend.zip &>>LOGFILE
-VALIDATE $? "Extracted backend code"
+# $? "Extracted backend code"
 
 npm install &>>LOGFILE
-VALIDATE $? "Installing Node js dependencies"
+# $? "Installing Node js dependencies"
 
 cp /home/ec2-user/Expenses/backend.service /etc/systemd/system/backend.service &>>LOGFILE
-VALIDATE $? "Copied backend service"
+# $? "Copied backend service"
 
 systemctl daemon-reload &>>$LOGFILE
-VALIDATE $? "Daemon Reload"
+# $? "Daemon Reload"
 
 systemctl start backend &>>$LOGFILE
-VALIDATE $? "Starting backend"
+# $? "Starting backend"
 
 systemctl enable backend &>>$LOGFILE
-VALIDATE $? "Enabling backend"
+# $? "Enabling backend"
 
 dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Installing MySql client"
+# $? "Installing MySql client"
 
 mysql -h db.chandureddy.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
-VALIDATE $? "Schema Loading"
+# $? "Schema Loading"
 
 systemctl restart backend &>>$LOGFILE
-VALIDATE $? "Restarting the backend"
+# $? "Restarting the backend"
