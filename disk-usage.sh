a279992130ba7f9cc0f44d90f8fc80c2489bf3a6
chandureddy08 <chandrareddyerigela@gmail.com>
2024-04-25 15:13:00 -0400

    Expense project

--- disk-usage.sh
+++ disk-usage.sh
@@ -8,7 +8,7 @@ DISK_THRESHOLD=6
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     FOLDER=$(echo $line | awk -F " " '{print $6F}')
-    if [ $USAGE -ge $DISK_THRESHOLD]
+    if [$USAGE -ge $DISK_THRESHOLD]
     then
         echo "$FOLDER is more than $DISK_THRESHOLD, current usage is: $USAGE"
     fi
