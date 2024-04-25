7951cb4b708c3ba0151d4bb3c9732b255c83a81b
chandureddy08 <chandrareddyerigela@gmail.com>
2024-04-25 15:11:48 -0400

    Expense project

---
+++ disk-usage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DISK_USGAE=$(df -ht | grep xfs)
+
+DISK_THRESHOLD=6
+
+ while IFS= read -r line
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    FOLDER=$(echo $line | awk -F " " '{print $6F}')
+    if [ $USAGE -ge $DISK_THRESHOLD]
+    then
+        echo "$FOLDER is more than $DISK_THRESHOLD, current usage is: $USAGE"
+    fi
+
+
+done <<< $DISK_USAGE
